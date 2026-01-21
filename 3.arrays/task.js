6f5daa7930fc79b1c3493b09a0b91e07dcb74c5e
Anna Bystrova <annbystr@gmail.com>
2026-01-21 16:06:55 +0300

    add third homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
+"use strict";
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const ages = users
+    .filter((user) => user.gender === gender)
+    .map((user) => user.age);
+
+  return ages.length
+    ? ages.reduce((acc, age) => acc + age, 0) / ages.length
+    : 0;
 }
